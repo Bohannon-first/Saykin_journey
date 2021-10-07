7fa9d60c36ac283b414af00eafc8880dab422501
Ярослав <spartacus215@yandex.ru>
2021-10-07 15:11:05 +0300

    Проверил верстку на переполнение контентом

--- gulpfile.js
+++ gulpfile.js
@@ -62,6 +62,7 @@ gulp.task('server', function () {
   });
 
   gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
+  gulp.watch('source/js/script.js', gulp.series('scripts'));
   gulp.watch('source/img/icon-*.svg', gulp.series('sprite', 'html', 'refresh'));
   gulp.watch('source/*.html', gulp.series('html', 'refresh'));
 });
